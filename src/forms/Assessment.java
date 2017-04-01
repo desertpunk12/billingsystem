26e78c926dd140d56fdaa5ff9a3fc796f36cb04c
desertpunk12 <chan2x12@gmail.com>
2017-04-02 05:16:02 +0800

    Added clearance and permit view plus refactored some code

--- src/forms/Assessment.java
+++ src/forms/Assessment.java
@@ -19,8 +19,6 @@ import java.util.HashMap;
 
 public class Assessment {
 
-    private boolean isAdmin;
-
     //<editor-fold defaultstate="collapsed" desc="UI Declarations">
     private JFrame frame;
     private JPanel pnlMain;
@@ -37,15 +35,22 @@ public class Assessment {
     private JPanel pnlAssessmentView;
     private JButton btnEditAssessmentView;
     private JScrollPane scrlSummary;
+    private JPanel pnlPermitView;
+    private JPanel pnlClearanceView;
     //</editor-fold>
+
+    private boolean isAdmin;
 
     private Student currentStudent;
     private Student prevStudent;
 
     private Thread thrdAssessment;
+    private Thread thrdPermit;
+    private Thread thrdClearance;
     private volatile boolean running = false;
     private volatile String studid;
 
+    private JLabel lblLoading;
     public Assessment(boolean isAdmin) {
         this.isAdmin = isAdmin;
         System.out.println(isAdmin);
@@ -53,51 +58,13 @@ public class Assessment {
         listeners();
         btnSearchStudentByName.setFocusable(false);//should change this latur
         btnFees.setFocusable(false);//should change this latur
-        txtSearchStudentIdNumber.requestFocus();
-        scrlSummary.getVerticalScrollBar().setUnitIncrement(16);
-
-        txtSearchStudentIdNumber.setToolTipText("You Can Press to move the focus here!");
-
     }
 
-    public synchronized void viewReport(String studid) throws JRFillInterruptedException{
-        try {
-            running = true;
-            String sourceFile = "src/jasperforms/COB.jrxml";
-            String sourceFileCompiled = "src/jasperforms/COB.jasper";
-            HashMap m = new HashMap();
-            m.put("name",currentStudent.getFullName());
-            m.put("studid",currentStudent.getStudId());
-            m.put("yrandcourse","Year "+currentStudent.getYrlvl()+" in "+currentStudent.getCourse());
-            m.put("sysem",currentStudent.getSem()+" "+currentStudent.getSy());
-            m.put("curdate",currentStudent.getDate());
-            m.put("subjectsDataSource",currentStudent.getSubjectsDataSource());
-            m.put("feesDataSource",currentStudent.getFeesDataSource());
-
-//            JasperReport jr = JasperCompileManager.compileReport(sourceFile);
-            JasperPrint jp = JasperFillManager.fillReport(sourceFileCompiled,m,DB.getConnection());
-            JRViewer pnlAssessmentReport = new JRViewer(jp);
-            SwingUtilities.invokeLater(()->{
-
-                for(int i=pnlAssessmentView.getComponentCount()-1;i>=0;i--){
-                    pnlAssessmentView.remove(i);
-                }
-
-                pnlAssessmentView.add(pnlAssessmentReport);
-                pnlAssessmentView.updateUI();
-                pnlAssessmentView.repaint();
-                running = false;
-            });
-
-        } catch (JRException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
     private void listeners(){
 
         btnEditAssessmentView.addActionListener((e)->{
-            EditAssessmentView editView = new EditAssessmentView();
+            EditAssessment editView = new EditAssessment();
             editView.show();
         });
 
@@ -145,9 +112,7 @@ public class Assessment {
         });
 
 
-        btnSearchStudentIdNumber.addActionListener(e -> {
-            showAssessment();
-        });
+        btnSearchStudentIdNumber.addActionListener(e -> assessStudent());
 
         pnlMain.addComponentListener(new ComponentAdapter() {
             @Override
@@ -159,68 +124,160 @@ public class Assessment {
 
     }
 
-    //<editor-fold defaultstate="collapsed" desc="Printables">
-    //Needs to change latur
-    public void showAssessment(){
+    //<editor-fold defaultstate="collapsed" desc="Assessing">
+     private synchronized void viewReport(JPanel pnl, String sourceFile, HashMap m, boolean compiled) throws JRFillInterruptedException,SQLException,JRException {
+        JasperPrint jp;
+        if (compiled){
+            jp = JasperFillManager.fillReport(sourceFile, m, DB.getConnection());
+        }else{
+            JasperReport jr = JasperCompileManager.compileReport(sourceFile);
+            jp = JasperFillManager.fillReport(jr, m, DB.getConnection());
+        }
+        JRViewer pnlAssessmentReport = new JRViewer(jp);
+        SwingUtilities.invokeLater(()-> {
+            for(int i=pnlAssessmentView.getComponentCount()-1;i>=0;i--){
+                pnlAssessmentView.remove(i);
+            }
+
+            pnlAssessmentView.add(pnl);
+            pnlAssessmentView.updateUI();
+            pnlAssessmentView.repaint();
+            running = false;
+        });
+
+    }
+
+
+    public synchronized void viewAssessmentReport(String studid) throws JRFillInterruptedException{
+        try {
+            running = true;
+            String sourceFile = "src/jasperforms/COB.jrxml";
+            String sourceFileCompiled = "src/jasperforms/COB.jasper";
+            HashMap m = new HashMap();
+            m.put("name",currentStudent.getFullName());
+            m.put("studid",currentStudent.getStudId());
+            m.put("yrandcourse","Year "+currentStudent.getYrlvl()+" in "+currentStudent.getCourse());
+            m.put("sysem",currentStudent.getSem()+" "+currentStudent.getSy());
+            m.put("curdate",currentStudent.getDate());
+            m.put("subjectsDataSource",currentStudent.getSubjectsDataSource());
+            m.put("feesDataSource",currentStudent.getFeesDataSource());
+            m.put("remainingbalance",currentStudent.getRemainingBalance());
+            m.put("scholarship",currentStudent.getScholarship());
+
+            viewReport(pnlAssessmentView,sourceFileCompiled,m,true);
+        } catch (JRException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public synchronized void viewPermitReport(String studid) throws JRFillInterruptedException{
+        try {
+            running = true;
+            String sourceFile = "src/jasperforms/Permit.jrxml";
+            String sourceFileCompiled = "src/jasperforms/Permit.jasper";
+            HashMap m = new HashMap();
+            m.put("name",currentStudent.getFullName());
+            m.put("studid",currentStudent.getStudId());
+            m.put("yrlvl",currentStudent.getYrlvl());
+            m.put("sysem",currentStudent.getSem()+" "+currentStudent.getSy());
+            m.put("subjectsDataSource","");
+            m.put("remainingbalance",currentStudent.getRemainingBalance());
+            m.put("scholarship",currentStudent.getScholarship());
+            m.put("minamountpayable","");
+
+
+            viewReport(pnlPermitView,sourceFileCompiled,m,true);
+        } catch (JRException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void viewClearanceReport(String studid) throws JRFillInterruptedException{
+         try {
+            running = true;
+            String sourceFile = "src/jasperforms/Clearance.jrxml";
+            String sourceFileCompiled = "src/jasperforms/Clearance.jasper";
+            HashMap m = new HashMap();
+            m.put("name",currentStudent.getFullName());
+            m.put("studid",currentStudent.getStudId());
+            m.put("yrlvl",currentStudent.getYrlvl());
+            m.put("sysem",currentStudent.getSem()+" "+currentStudent.getSy());
+            m.put("subjectsDataSource","");
+            m.put("remainingbalance",currentStudent.getRemainingBalance());
+            m.put("scholarship",currentStudent.getScholarship());
+            m.put("minamountpayable","");
+
+            viewReport(pnlClearanceView,sourceFileCompiled,m,true);
+        } catch (JRException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void assessStudent(){
         String tmpstudid = txtSearchStudentIdNumber.getText();
         if(tmpstudid.equals(studid))
             return;
-
         studid = tmpstudid;
-
-
         if(currentStudent!=null)
             prevStudent = currentStudent;
         currentStudent = new Student(studid);
-        if(!running && pnlAssessmentView.getComponentCount()<2) {
-            JLabel lblLoading = new JLabel();
-            lblLoading.setText("Loading Report View . . .");
-            pnlAssessmentView.add(lblLoading);
-            pnlAssessmentView.updateUI();
-            pnlAssessmentView.repaint();
-        }
+
+        addLoading(pnlAssessmentView);
+        addLoading(pnlPermitView);
+        addLoading(pnlClearanceView);
+
         if(running) {
             thrdAssessment.interrupt();
+            thrdPermit.interrupt();
+            thrdClearance.interrupt();
             running = false;
-        }else
-//            try {
-//                thrdAssessment.join(500);
-//                running = false;
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+        }
 
-        thrdAssessment = new Thread(() -> {
-            try{
-                viewReport(studid);
-            }catch (JRFillInterruptedException e){
-                System.out.println("Cancelled jasper view request!");
-            }
-        });
+        thrdAssessment = new Thread(() -> {try{viewAssessmentReport(studid);}catch (JRFillInterruptedException e){System.out.println("Cancelled jasper view request!");}});
         thrdAssessment.start();
+
+        thrdPermit = new Thread(() -> {try{viewPermitReport(studid);}catch (JRFillInterruptedException e){System.out.println("Cancelled jasper view request!");}});
+        thrdPermit.start();
+
+        thrdClearance= new Thread(() -> {try{viewClearanceReport(studid);}catch (JRFillInterruptedException e){System.out.println("Cancelled jasper view request!");}});
+        thrdClearance.start();
+
         //TODO: removthis shit
         showSummaryAssessment();
     }
 
-
-
-    public void showPermit(){
-
+    private void addLoading(JPanel pnl){
+         if(!running&& pnl.getComponentCount()<2) {
+            lblLoading = new JLabel();
+            lblLoading.setText("Loading Report View . . .");
+            pnl.add(lblLoading);
+            pnl.updateUI();
+            pnl.repaint();
+        }
     }
 
-
-    public void showClearance(){}
-    //</editor-fold>
-
-    public void showSummaryAssessment(){
+    private void showSummaryAssessment(){
         SummaryAssessmentSy sssy = new SummaryAssessmentSy("2013 - 2014");
 
         sssy.attach(pnlSummary);
     }
 
+    //</editor-fold>
+
     private void init(){
+        frame = new JFrame();
+
         setName();
+
         pnlAssessmentView.setLayout(new BoxLayout(pnlAssessmentView,BoxLayout.PAGE_AXIS));
+        pnlPermitView.setLayout(new BoxLayout(pnlPermitView,BoxLayout.PAGE_AXIS));
+        pnlClearanceView.setLayout(new BoxLayout(pnlClearanceView,BoxLayout.PAGE_AXIS));
+
+        txtSearchStudentIdNumber.requestFocus();
+        scrlSummary.getVerticalScrollBar().setUnitIncrement(16);
+        txtSearchStudentIdNumber.setToolTipText("You Can Press to move the focus here!");
     }
 
     //<editor-fold defaultstate=collapsed desc="No Change Needed">
@@ -237,18 +294,18 @@ public class Assessment {
 
     public void show(){
         JFrameHelper.show(frame,pnlMain,"Students Billing System of DOSCST",true);
-            SwingUtilities.invokeLater(() -> {
-                try{
-                    MaskFormatter formatter = new MaskFormatter("#### - ####");
-                    formatter.setPlaceholderCharacter('0');
-                    txtSearchStudentIdNumber.setFormatterFactory(new DefaultFormatterFactory(formatter));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            });
+        SwingUtilities.invokeLater(() -> {
+            try{
+                MaskFormatter formatter = new MaskFormatter("#### - ####");
+                formatter.setPlaceholderCharacter('0');
+                txtSearchStudentIdNumber.setFormatterFactory(new DefaultFormatterFactory(formatter));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        });
 
 
-        SwingUtilities.invokeLater(()-> splitPane.setDividerLocation(0.6));
+        SwingUtilities.invokeLater(()-> splitPane.setDividerLocation(0.8));
     }
     //</editor-fold>
 
